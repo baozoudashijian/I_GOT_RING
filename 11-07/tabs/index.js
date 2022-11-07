0f0ebe520104ab21962394b2c59a177f5f61ff4b
Roger Zhang <1787009521@qq.com>
2022-11-07 14:42:55 +0800

    feat: tabs-2

--- 11-07/tabs/index.js
+++ 11-07/tabs/index.js
@@ -1,15 +1,42 @@
 // 循环所有tab
-$('.tabs').each(function(index, element) {
-    $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
-    $(element).children('.tabs-content').children('li').eq(0).addClass('active')
-})
+// $('.tabs').each(function(index, element) {
+//     $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
+//     $(element).children('.tabs-content').children('li').eq(0).addClass('active')
+// })
 
-$('.tabs').on('click', '.tabs-bar > li', function(e) {
-    var $li = $(e.currentTarget)
+// $('.tabs').on('click', '.tabs-bar > li', function(e) {
+//     var $li = $(e.currentTarget)
 
-    $li.addClass('active').siblings().removeClass('active')
-    var index = $li.index()
-    // 从里面往外面找
-    var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
-    $content.addClass('active').siblings().removeClass('active')
-})
+//     $li.addClass('active').siblings().removeClass('active')
+//     var index = $li.index()
+//     // 从里面往外面找
+//     var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
+//     $content.addClass('active').siblings().removeClass('active')
+// })
+
+function Tabs(selector) {
+    this.element = $(selector)
+    this.init()
+    this.bindEvents()
+}
+
+Tabs.prototype.init = function() {
+    this.element.each(function(index, element) {
+        $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
+        $(element).children('.tabs-content').children('li').eq(0).addClass('active')
+    })
+}
+
+Tabs.prototype.bindEvents = function() {
+    this.element.on('click', '.tabs-bar > li', function(e) {
+        var $li = $(e.currentTarget)
+    
+        $li.addClass('active').siblings().removeClass('active')
+        var index = $li.index()
+        // 从里面往外面找
+        var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
+        $content.addClass('active').siblings().removeClass('active')
+    })
+}
+
+var tabs = new Tabs('.tabs')
