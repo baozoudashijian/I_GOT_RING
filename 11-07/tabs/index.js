3dc12d49880d6ff39515efe291470c5304d19fd5
Roger Zhang <1787009521@qq.com>
2022-11-07 14:45:46 +0800

    feat: tabs-3

--- 11-07/tabs/index.js
+++ 11-07/tabs/index.js
@@ -14,29 +14,54 @@
 //     $content.addClass('active').siblings().removeClass('active')
 // })
 
-function Tabs(selector) {
-    this.element = $(selector)
-    this.init()
-    this.bindEvents()
-}
+// function Tabs(selector) {
+//     this.element = $(selector)
+//     this.init()
+//     this.bindEvents()
+// }
 
-Tabs.prototype.init = function() {
-    this.element.each(function(index, element) {
-        $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
-        $(element).children('.tabs-content').children('li').eq(0).addClass('active')
-    })
-}
+// Tabs.prototype.init = function() {
+//     this.element.each(function(index, element) {
+//         $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
+//         $(element).children('.tabs-content').children('li').eq(0).addClass('active')
+//     })
+// }
 
-Tabs.prototype.bindEvents = function() {
-    this.element.on('click', '.tabs-bar > li', function(e) {
-        var $li = $(e.currentTarget)
+// Tabs.prototype.bindEvents = function() {
+//     this.element.on('click', '.tabs-bar > li', function(e) {
+//         var $li = $(e.currentTarget)
     
-        $li.addClass('active').siblings().removeClass('active')
-        var index = $li.index()
-        // 从里面往外面找
-        var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
-        $content.addClass('active').siblings().removeClass('active')
-    })
+//         $li.addClass('active').siblings().removeClass('active')
+//         var index = $li.index()
+//         // 从里面往外面找
+//         var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
+//         $content.addClass('active').siblings().removeClass('active')
+//     })
+// }
+
+class Tabs {
+    constructor(selector) {
+        this.element = $(selector)
+        this.init()
+        this.bindEvents()
+    }
+    init() {
+        this.element.each(function(index, element) {
+            $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
+            $(element).children('.tabs-content').children('li').eq(0).addClass('active')
+        })
+    }
+    bindEvents() {
+        this.element.on('click', '.tabs-bar > li', function(e) {
+            var $li = $(e.currentTarget)
+        
+            $li.addClass('active').siblings().removeClass('active')
+            var index = $li.index()
+            // 从里面往外面找
+            var $content = $li.closest('.tabs').find('.tabs-content > li').eq(index)
+            $content.addClass('active').siblings().removeClass('active')
+        })
+    }
 }
 
 var tabs = new Tabs('.tabs')
